d45e062f7d91e2b05793fb30ee2176fa43a5cff5
Jannik Wirtheim <147552858+wirthy21@users.noreply.github.com>
2025-02-21 13:38:06 +0100

    Update main_jannik.py

--- main_jannik.py
+++ main_jannik.py
@@ -128,7 +128,7 @@ def execute_instruction(data):
         start_time_turn = time.time()
         picam2.stop()
         data = None
-        while time.time() - start_time_turn < 1:
+        while time.time() - start_time_turn < 1.5:
             robot.changespeed(qr_turn_speed, qr_turn_speed)
             robot.turnRight()
         picam2.start()    
@@ -138,7 +138,7 @@ def execute_instruction(data):
         start_time_rotate = time.time()
         picam2.stop()
         data = None
-        while time.time() - start_time_rotate < 4:
+        while time.time() - start_time_rotate < 6:
             robot.changespeed(qr_turn_speed, qr_turn_speed)
             robot.turnRight()
         picam2.start()
